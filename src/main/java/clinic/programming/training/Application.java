3ef24330cdcf523d87e8ff33f145d6f79f0baafe
AymanElMikh <aymanmikh7@gmail.com>
2024-07-03 12:46:51 +0100

    costume compile plugin

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,9 +1,22 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
+    List<String> messages;
     public Application() {
         System.out.println ("Inside Application");
+        messages = new ArrayList<String>();
+
+        messages.add("Hello");
+        messages.add("World");
+
+        for (String message : messages){
+            System.out.println(message);
+        }
+
     }
 
     // method main(): ALWAYS the APPLICATION entry point
