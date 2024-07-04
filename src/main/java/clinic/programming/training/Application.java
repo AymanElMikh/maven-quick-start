e19accf2bef607cba5e05e25ee24828a13ddaa59
AymanElMikh <aymanmikh7@gmail.com>
2024-07-04 11:18:19 +0100

    Apache common lang3 dependencie added

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,10 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.commons.lang3.StringUtils;
 public class Application {
 
     List<String> messages;
+
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
     public Application() {
         System.out.println ("Inside Application");
         messages = new ArrayList<String>();
@@ -16,7 +21,8 @@ public class Application {
         for (String message : messages){
             System.out.println(message);
         }
-
+        String words =  "Word1 Word2 Word3";
+        System.out.println("The number of words in " + words + " is " + countWords(words));
     }
 
     // method main(): ALWAYS the APPLICATION entry point
